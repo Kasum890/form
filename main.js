98c5ac1477734cb5240919e010fa7177a2754d14
kasum <xasbik89@gmail.com>
2024-11-16 15:32:07 +0300

    three commit

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-const signInBtn = document.querySelector('.signin-btn')
-const signUpBtn = document.querySelector('.signup-btn')
-const formBox = document.querySelector('.form-box')
-const body = document.body
+document.addEventListener("DOMContentLoaded", () => {
+  const signInBtn = document.querySelector(".signin-btn");
+  const signUpBtn = document.querySelector(".signup-btn");
+  const formBox = document.querySelector(".form-box");
+  const body = document.body;
 
+  signUpBtn.addEventListener("click", () => {
+    formBox.classList.add("active");
+    body.classList.add("active");
+  });
 
-signUpBtn.addEventListener('click', () => {
-    formBox.classList.add('active')
-    body.classList.add('active')
-})
-
-signInBtn.addEventListener('click', () => {
-    formBox.classList.remove('active')
-    body.classList.remove('active')
-
-})
+  signInBtn.addEventListener("click", () => {
+    formBox.classList.remove("active");
+    body.classList.remove("active");
+  });
+});
